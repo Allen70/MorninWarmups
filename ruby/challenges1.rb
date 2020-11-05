7e86368134b36aac3b4f7684d46b4bd749551056
jjhiggz <jonathan.higger@gmail.com>
2020-11-05 09:56:45 -0700

    added ruby challenge

---
+++ ruby/challenges1.rb
@@ -0,0 +1,19 @@
+my_array = [ 1, 2, 3, 9000, 5 , 12, 1, 76, 42]
+
+# Sort my_array from least to greatest
+
+# Find the largest term in my_array
+
+# create a new array where each element is the element of my_array but if it is even, it becomes a 0, and if it is odd it becomes a 1
+
+# create a new Array where all odd numbers are filtered out, do not use the array of 0s and 1s
+
+my_string = 'hello my name is jonathan higger, and doomslayer is my buddy'
+
+# turn my_string into an array of words, save this in a new variable called my_string_array
+
+# create a copy of my_string_array, but if any term has 'doomslayer' as its value then replace it with 'DOOMSLAYER'. Save this into a variable called my_corrected_string_arrayu
+
+# Join the terms of my_corrected_string_array to a new string that should be equivalent to 'hello my name is jonathan higger, and DOOMSLAYER is my buddy' 
+
+
